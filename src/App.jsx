c77f6c64bf72afebb64d6eb9459ff4d1f8503c01
Niels Kesting <niels.kesting@gmail.com>
2023-04-18 10:58:18 +0200

    Finished the Props exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import "./App.css";
-import { DrinkButtons } from "./components/DrinkButtons";
+import React from 'react';
+import './App.css';
+import { DrinkButtons } from './components/DrinkButtons';
+import { DrinkChoice } from './components/DrinkChoice';
+import { tea, coffee } from './utils/data';
 
 export const App = () => {
-  const greeting = "Welcome to our cafe!";
+	const greeting = 'Welcome to our cafe!';
 
-  return (
-    <div className="App">
-      <h1>{greeting}</h1>
-      <DrinkButtons />
-    </div>
-  );
+	const userDrink = tea;
+
+	return (
+		<div className="app">
+			<h1>{greeting}</h1>
+			<DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+			<DrinkChoice drink={userDrink} />
+		</div>
+	);
 };
--- src/components/DrinkButtons.jsx
+++ src/components/DrinkButtons.jsx
@@ -1,13 +1,16 @@
-import React from "./DrinkButtons.css";
+import './DrinkButtons.css';
+import { Button } from './ui/Button';
 
-export const DrinkButtons = () => {
-  return (
-    <>
-      <h2>Would you like tea or coffee?</h2>
-      <div className="button-group">
-        <button className="button">Tea</button>
-        <button className="button">Coffee</button>
-      </div>
-    </>
-  );
+export const DrinkButtons = ({ drinkOne, drinkTwo }) => {
+	return (
+		<>
+			<h2>
+				Would you like {drinkOne} or {drinkTwo}?
+			</h2>
+			<div className="button-group">
+				<Button text={drinkOne} />
+				<Button text={drinkTwo} />
+			</div>
+		</>
+	);
 };
